226177dc02c83b94677be474cd41f248a1da0acd
testingAlbatross <137203877+testingAlbatross@users.noreply.github.com>
2023-06-21 15:33:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 async function displayForm() {
-    const res = await fetch('http://localhost:3000/access');
+    const res = await fetch('http://localhost:3000/access', {
+        method: "GET",
+        mode: "cors",
+        cache: "no-cache",
+        credentials: "same-origin",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
     const data = await res.json();
     console.log(data);
     if (data.exists)
