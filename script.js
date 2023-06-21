0daeae6a34c008e400c68b791a12516965c5f87f
testingAlbatross <137203877+testingAlbatross@users.noreply.github.com>
2023-06-21 15:06:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+async function displayForm() {
+    const res = await fetch('http://localhost:3000/access');
+    const data = await res.json();
+
+    if (data.exists)
+    {   
+        const resp = 
+        document.getElementById('login').innerHTML = '<h1>Username exists</h1>';
+    }
+    else
+    {
+        document.getElementById('login').innerHTML = '<h1>Login with github</h1>';
+    }
+}
